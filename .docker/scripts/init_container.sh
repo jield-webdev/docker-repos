379de50617c0631378cdffb3501f010fcf132b29
Johan van der Heide <info@jield.nl>
2021-12-12 12:26:07 +0100

    Cleanup of all scripts, include azure versions

---
+++ .docker/scripts/init_container.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cat >/etc/motd <<EOL
+
+      _ ___ _____ _     ____    ______     __
+     | |_ _| ____| |   |  _ \  | __ ) \   / /
+  _  | || ||  _| | |   | | | | |  _ \\ \ / /
+ | |_| || || |___| |___| |_| | | |_) |\ V /
+  \___/|___|_____|_____|____/  |____/  \_/
+
+Jield BV Docker container
+
+by: Johan van der Heide - info@jield.nl
+
+EOL
+cat /etc/motd
+
+/usr/sbin/sshd
