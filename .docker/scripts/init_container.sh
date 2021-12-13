30a8f5a731a3c08dbe6f4532456b27e2bf33778b
Johan van der Heide <info@jield.nl>
2021-12-13 23:03:35 +0100

    Used a template from MS to make the container and to toggle some other keys
    Signed-off-by: Johan van der Heide <info@jield.nl>

--- .docker/scripts/init_container.sh
+++ .docker/scripts/init_container.sh
@@ -14,4 +14,10 @@ by: Johan van der Heide - info@jield.nl
 EOL
 cat /etc/motd
 
+# Get environment variables to show up in SSH session
+# shellcheck disable=SC2046
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+# starting sshd process
+sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
 /usr/sbin/sshd
