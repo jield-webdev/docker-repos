2a56e01d7d9e822324ce4aca1fae3eaa758332ef
Johan van der Heide <info@jield.nl>
2021-12-15 19:44:01 +0100

    Make sure the nginx container has a working SSH connection
    Signed-off-by: Johan van der Heide <info@jield.nl>

--- .docker/scripts/init_container.sh
+++ .docker/scripts/init_container.sh
@@ -16,8 +16,6 @@ cat /etc/motd
 
 # Get environment variables to show up in SSH session
 # shellcheck disable=SC2046
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >>/etc/profile)
 
-# starting sshd process
-sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-/usr/sbin/sshd
+exec /usr/sbin/sshd -D -e "$@"
