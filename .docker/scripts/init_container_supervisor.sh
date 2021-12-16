b1a94e9b612f72a725d971eee7a654b0b390b42b
Johan van der Heide <info@jield.nl>
2021-12-17 00:08:22 +0100

    Add user=root to the supervisord.conf to avoid the errors And return of the script which sets the environmental vars
    Signed-off-by: Johan van der Heide <info@jield.nl>

--- .docker/scripts/init_container_supervisor.sh
+++ .docker/scripts/init_container_supervisor.sh
@@ -14,8 +14,14 @@ by: Johan van der Heide - info@jield.nl
 EOL
 cat /etc/motd
 
-printenv > /etc/environment
+# This function adds export commands for all server params to the profile page
+# export FOOBAR_VAR='value' so these values are generally available in the container
+# this is mainly important for CLI cronjobs
+# shellcheck disable=SC2046
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+# Create a file to which cron logs can be written
 touch /var/log/cron.log
 
+# Start the supervisor
 /usr/bin/supervisord
